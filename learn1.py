c4dcd67231e0c6257d6196931206e252bc8c8094
Valentin <valin223@gmail.com>
2019-02-14 23:28:47 +0300

    Add comments to file learn1

--- learn1.py
+++ learn1.py
@@ -1,14 +1,19 @@
-
+# int
 myNumber = 10
+
+# float
 myNumberFloat = 10.0147
 
+# str
 myString = 'this is a string'
 
+# list
 myList = [
     myNumber,
     myString
 ]
 
+# dict
 myDict = {
     'width': 800,
     'height': 600,
@@ -16,11 +21,11 @@ myDict = {
     'motion-blur': False
 }
 
-
+# function
 def add(arg1, arg2):
     return arg1 + arg2
 
-
+# class Car
 class Car:
     def __init__(self):
         self.engine = 'V 6 750 hp'
